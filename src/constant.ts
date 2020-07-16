104167f7ad4b3f98e35774bb4f4c3ee30a33d992
nerianighthawk <hiroya.nakakawaji@gmail.com>
2020-07-16 17:38:04 +0900

    fix api host ip.

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
 export const APP_TITLE = 'TRAHA Event Manager'
 
-export const URI_HOST = 'http://13.181.186.52:8000/'
+export const URI_HOST = 'http://18.181.186.52/'
 // export const URI_HOST = 'http://localhost:8000/'
 export const URI_API = URI_HOST + 'api/'
 export const URI_API_EVENT = URI_API + 'event/'
